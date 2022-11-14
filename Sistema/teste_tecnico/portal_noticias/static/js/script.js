710d7a7ccc88907cbe0c642f7b9eb32f8ad9fc65
krupck <krupique@outlook.com>
2022-11-14 11:11:50 -0300

    STYLE: Atualização do estilo da página

--- Sistema/teste_tecnico/portal_noticias/static/js/script.js
+++ Sistema/teste_tecnico/portal_noticias/static/js/script.js
@@ -26,9 +26,20 @@ $(document).ready(function(){
 
                 var strlHtml = '<div id="post-enviado">Seu comentário foi enviado com sucesso</div>';
                 $('#form-comment').html(strlHtml);
-
                 
-                strlHtml = `<li id="last-comment" class="li-comment">${data.name} Now - ${data.comment}</li>`;
+                
+                strlHtml = '<div class="row" id="last-comment">';
+                strlHtml += '<div class="col-1"><img src="../static/assets/user.png" width="50px"></div>';
+                strlHtml += '<div class="col-11">';
+                strlHtml += '<div class="row">';
+                strlHtml += `<div class="col-3 comment-name"><p>${data.name}</p></div>`;
+                strlHtml += `<div class="col-4 comment-date"><span><i class="fas fa-clock"></i>Agora</span></div>`;
+                strlHtml += `<div class="col-12 comment-comment"><p>${data.comment}</p></div>`;
+                strlHtml += '</div>';
+                strlHtml += '</div>';
+                strlHtml += '</div>';
+                strlHtml += '<hr/>';
+
                 $('#list-comments').prepend(strlHtml);
 
                 
@@ -55,15 +66,26 @@ $(document).ready(function(){
             data: data, 
             success: function (response) {
                 list_comments = JSON.parse(response.instance)
-                
-                console.log(list_comments);
-
-
-
 
                 paginationNumber += 20;
 
+                var strlHtml = ''
+                list_comments.forEach(e => {
 
+                    strlHtml += '<div class="row">';
+                    strlHtml += '<div class="col-1"><img src="../static/assets/user.png" width="50px"></div>';
+                    strlHtml += '<div class="col-11">';
+                    strlHtml += '<div class="row">';
+                    strlHtml += `<div class="col-3 comment-name"><p>${e.fields.name}</p></div>`;
+                    strlHtml += `<div class="col-4 comment-date"><span><i class="fas fa-clock"></i>${e.fields.created_at}</span></div>`;
+                    strlHtml += `<div class="col-12 comment-comment"><p>${e.fields.comment}</p></div>`;
+                    strlHtml += '</div>';
+                    strlHtml += '</div>';
+                    strlHtml += '</div>';
+                    strlHtml += '<hr/>';
+                });
+
+                $('#list-comments').html(strlHtml);
                 
             },
             error: function (response) {
